ecc0aeb5a597a9f44835c4aef2fed7cbeb53c622
Brett Hosking <brett.hosking@gmail.com>
2019-02-05 16:38:57 +0000

    setup package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='dummypy',
+      version='0.1.0',
+      description='A dummy python package template',
+      url='http://github.com/brett-hosking/dummypy',
+      author='brett hosking',
+      author_email='wilski@noc.ac.uk',
+      packages=['dummypy','dummypy.packageA','dummypy.packageB'])
